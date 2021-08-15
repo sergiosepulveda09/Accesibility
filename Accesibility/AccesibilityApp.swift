fa42c0ce5a1e659f2bcebfa9a6997cd211560883
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-08-15 13:00:24 -0700

    Initial Commit

---
+++ Accesibility/AccesibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibilityApp.swift
+//  Accesibility
+//
+//  Created by Sergio Sepulveda on 2021-08-15.
+//
+
+import SwiftUI
+
+@main
+struct AccesibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
